1e671e68ada108330359c02fc821b4c38ad20ec2
danielcosta4 <daniel.costa@artefatodigital.com>
2025-09-10 16:24:23 -0300

    feat: criar rota/método para cadastro de candidato.

---
+++ src/main/java/br/com/danielcosta/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,19 @@
+package br.com.danielcosta.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.danielcosta.gestao_vagas.modules.candidate.CandidateEntity;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    // Recebe do body, tipo CandidateEntity
+    public void create(@RequestBody CandidateEntity candidate) {
+        System.out.println(String.format("Candidato: %s criado com sucesso!", candidate.getEmail()));
+    }
+}
